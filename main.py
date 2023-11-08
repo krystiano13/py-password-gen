0a03081b1f841598b3f2fee338c5fadc578df9fd
krystiano13 <crystianotv@gmail.com>
2023-11-08 11:11:18 +0100

    move ask functions to different module

--- main.py
+++ main.py
@@ -1,38 +1,9 @@
+from info import askNumbers, askSpecial, askUpperCase
+
 charAmount = 0
 containUpperCase = False
 containNumbers = False
 containSpecial = False
-
-def askSpecial():
-    spec = input("Should your password contain special characters ? [true/false]")
-    if spec == "true":
-        return True
-    elif spec == "false":
-        return False
-    else:
-        print("Wrong answer provided")
-        return askSpecial()
-
-def askNumbers():
-    num = input("Should your password contain number characters ? [true/false]")
-    if num == "true":
-        return True
-    elif num == "false":
-        return False
-    else:
-        print("Wrong answer provided")
-        return askNumbers()
-
-def askUpperCase():
-    upper = input("Should your password contain upperCase characters ? [true/false]")
-
-    if upper == "true":
-        return True
-    elif upper == "false":
-        return False
-    else:
-        print("Wrong answer provided")
-        return askUpperCase()
 
 def getCharAmount():
     print("How many characters your password should have ? \n")
